20f5d709cb879e2674ef1a797f2e150b93de579a
EmmanuelArethens <emmanuel.arethens1@gmail.com>
2017-09-07 17:20:33 +0200

    données récupérées

--- main.js
+++ main.js
@@ -1,6 +1,18 @@
-var owjs = require('overwatch-js');
+const express = require('express');
+const app = express();
+
+app.listen('3000', function(){
+    console.log('server listening on port 3000')
+})
+
+
+let owjs = require('overwatch-js');
 owjs.getAll('pc', 'eu', 'Lanikeu-21211').then(
     function (data) {
-        console.log(data)
+        console.log(data.profile.rankPicture)
     }
 );
+
+app.set('views', './template');
+app.set('view engine', 'html');
+app.use("/", express.static("static"));
