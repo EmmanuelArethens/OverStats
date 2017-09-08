363e1ffa931fa1183662b95ffcad559ecd0d323f
EmmanuelArethens <emmanuel.arethens1@gmail.com>
2017-09-08 17:50:09 +0200

    requete OK

--- main.js
+++ main.js
@@ -1,18 +1,38 @@
 const express = require('express');
 const app = express();
+const bodyParser = require('body-parser');
+let owjs = require('overwatch-js');
+let name = 'Lanikeu-21211'
+let plateform = 'pc'
+let region = 'eu'
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({ extended:true }));
+
+// redirection vers index
+app.get("/", function(req, resp){
+resp.sendFile(__dirname + '/index.html')
+});
+
+app.post("/getstats", function(req, resp){
+    owjs.getAll(req.body.plateform, req.body.region, req.body.name).then(
+        function (data) {
+            resp.send(data)
+        }
+    );
+})
 
 app.listen('3000', function(){
     console.log('server listening on port 3000')
 })
 
-
-let owjs = require('overwatch-js');
-owjs.getAll('pc', 'eu', 'Lanikeu-21211').then(
+owjs.getAll(plateform, region, name).then(
     function (data) {
-        console.log(data.profile.rankPicture)
+        console.log(data)
     }
 );
 
-app.set('views', './template');
-app.set('view engine', 'html');
-app.use("/", express.static("static"));
+app.post("/add", function(req, resp) {
+    resp.send("ok");
+    console.log(req.data.profile);
+ });
+
